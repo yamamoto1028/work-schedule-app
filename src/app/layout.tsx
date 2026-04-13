1476ee71a3d7b40340eb78a7a445088f5a890e8b
Shuhei Yamamoto <129731756+yamamoto1028@users.noreply.github.com>
2026-04-13 23:10:14 +0900

    🐛

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
